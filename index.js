010c1628bb43e175d75f8c9af4f0b9745b2149ff
Susan Lockwood <susan.m.mullin@gmail.com>
2018-08-02 05:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,12 +30,3 @@ function takeANumber(line, name){
   
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
-takeANumber(katzDeli, "Ada")
-takeANumber(katzDeli, "Grace")
-takeANumber(katzDeli, "Kent")
-currentLine(katzDeli);
-nowServing(katzDeli);
-takeANumber(katzDeli, "Matz"); 
-currentLine(katzDeli); 
-nowServing(katzDeli);
-currentLine(katzDeli)
